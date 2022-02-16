726812b3b84040e917a09232d29efc1b8f4034c6
Florent Maury <flo.neo@hotmail.fr>
2022-02-16 10:52:21 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+        // DÉCLARATION DES VARIABLES.
+
+let playerOne, playerOneGlobalScore, PlayerOneCurrentScore,
+    playerTwo, playerTwoGlobalScore, PlayerTwoCurrentScore,
+    newGame, rollDice, hold, dice;
+
+        // RÉCUPÉRATION DES ÉLÉMENTS DE LA PAGE HTML.
+
+playerOne = document.querySelector('#playerOne');
