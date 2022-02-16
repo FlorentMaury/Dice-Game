ffa94eabfc10ec5bd6c83a7919c00bb166ddac9e
Florent Maury <flo.neo@hotmail.fr>
2022-02-16 15:04:09 +0100

    Avancée avec un seul joueur.

--- script/script.js
+++ script/script.js
@@ -2,7 +2,9 @@
 
 let playerOne, playerOneGlobalScore, PlayerOneCurrentScore,
     playerTwo, playerTwoGlobalScore, PlayerTwoCurrentScore,
-    newGame, rollDice, hold, dice, last, randomNumber;
+    newGame, rollDice, hold, dice, randomNumber;
+let last = 0;
+let global = 0;
 
     // RÉCUPÉRATION DES ÉLÉMENTS DE LA PAGE HTML.
 
@@ -21,30 +23,44 @@ PlayerTwoCurrentScore = document.querySelector('#PlayerTwoCurrentScore');
 
 function diceNumber() {
     randomNumber = Math.floor(Math.random() * 6) + 1;
+    dice.innerHTML = `<img src="images/${randomNumber}.png" alt="Image du dé">`;
 }
 
 function init() {
-
     diceNumber();
-
-    dice.innerHTML = `<img src="images/${randomNumber}.png" alt="Image du dé">`
-    playerOne.innerHTML   = 'PLAYER 1 <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-dot text-danger" viewBox="0 0 16 16"><path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/></svg>';
-    last = 0 + randomNumber;
+    playerOne.innerHTML   = 'PLAYER 1 <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-dot text-danger" viewBox="0 0 16 16"><path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/></svg>';
+    playerOne.style.fontWeight = 300;
     if (randomNumber !== 1) {
         last += randomNumber;
-        PlayerOneCurrentScore.textContent = roundScore;
+        PlayerOneCurrentScore.textContent = last;
     } else {
+        PlayerOneCurrentScore.textContent = 0;
+        last = 0;
     }
-
 }
 
-    // AJOUT DES ÉVÈNEMENTS ET ÉCOUTEURS.
+function save() {
+    global += last
+    playerOneGlobalScore.textContent = global;
+    last = 0;
+    PlayerOneCurrentScore.textContent = 0;
+    if (global >= 100) {
+        alert(`C'est gagné !`);
+    }
+}
 
-newGame.addEventListener('click', () => {
+function newRound() {
     playerOneGlobalScore.textContent  = 0;
     playerTwoGlobalScore.textContent  = 0;
     PlayerOneCurrentScore.textContent = 0;
     PlayerTwoCurrentScore.textContent = 0;
-});
+    last = 0;
+}
 
-rollDice.addEventListener('click', init)
+    // AJOUT DES ÉVÈNEMENTS ET ÉCOUTEURS.
+
+newGame.addEventListener('click', newRound);
+
+rollDice.addEventListener('click', init);
+
+hold.addEventListener('click', save);
