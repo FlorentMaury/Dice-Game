7192c1d25dd2e0f6ba37aadab3ad02bee83f9915
Florent Maury <flo.neo@hotmail.fr>
2022-02-17 13:10:46 +0100

    Début idée changement de joueur avec variables booleènnes.

--- script/script.js
+++ script/script.js
@@ -2,9 +2,11 @@
 
 let playerOne, playerOneGlobalScore, PlayerOneCurrentScore,
     playerTwo, playerTwoGlobalScore, PlayerTwoCurrentScore,
-    newGame, rollDice, hold, dice, randomNumber;
+    newGame, rollDice, hold, dice, randomNumber; 
 let last = 0;
 let global = 0;
+let playerOneTurn = true;
+let playerTwoTurn = false
 
     // RÉCUPÉRATION DES ÉLÉMENTS DE LA PAGE HTML.
 
@@ -28,14 +30,39 @@ function diceNumber() {
 
 function init() {
     diceNumber();
-    playerOne.innerHTML   = 'PLAYER 1 <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-dot text-danger" viewBox="0 0 16 16"><path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/></svg>';
-    playerOne.style.fontWeight = 300;
-    if (randomNumber !== 1) {
-        last += randomNumber;
-        PlayerOneCurrentScore.textContent = last;
-    } else {
-        PlayerOneCurrentScore.textContent = 0;
-        last = 0;
+
+    // playerOne.innerHTML   = 'PLAYER 1 <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-dot text-danger" viewBox="0 0 16 16"><path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/></svg>';
+    // playerOne.style.fontWeight = 300;
+    // if (randomNumber !== 1) {
+    //     last += randomNumber;
+    //     PlayerOneCurrentScore.textContent = last;
+    // } else {
+    //     PlayerOneCurrentScore.textContent = 0;
+    //     last = 0;
+    // }
+
+    if (playerOneTurn) {
+        if (randomNumber !== 1) {
+            last += randomNumber;
+            PlayerOneCurrentScore.textContent = last;
+        } else {
+            PlayerOneCurrentScore.textContent = 0;
+            last = 0;
+            playerOneTurn = false;
+            playerTwoTurn = true;
+        }
+    }
+
+    if (playerTwoTurn) {
+        if (randomNumber !== 1) {
+            last += randomNumber;
+            PlayerTwoCurrentScore.textContent = last;
+        } else {
+            PlayerTwoCurrentScore.textContent = 0;
+            last = 0;
+            playerOneTurn = true;
+            playerTwoTurn = false;
+        }
     }
 }
 
